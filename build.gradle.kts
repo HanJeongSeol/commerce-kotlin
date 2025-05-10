6a838d4bf5ca91a47b510a637ea7c507a8fae869
HanJeongSeol <hanjeong94@naver.com>
2025-05-10 12:34:46 +0900

    [SETTING] dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    kotlin("plugin.jpa") version "2.1.0"
     id("jacoco")
 }
 
@@ -19,11 +20,25 @@ dependencyManagement {
 }
 
 dependencies {
+    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spring Boot & JPA (기존 세팅 반영)
     implementation(libs.spring.boot.starter.web)
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     annotationProcessor(libs.spring.boot.configuration.processor)
+
+    // DB (기존 세팅 반영)
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Test (기존 Testcontainers 설정 포함)
     testImplementation(libs.spring.boot.starter.test)
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 // about source and compilation
--- src/main/kotlin/io/hhplus/tdd/TddApplication.kt
+++ src/main/kotlin/io/hhplus/tdd/TddApplication.kt
@@ -1,9 +1,11 @@
 package io.hhplus.tdd
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+// (exclude = [DataSourceAutoConfiguration::class]) : DB 없이 서버 띄우기
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class TddApplication
 
 fun main(args: Array<String>) {
